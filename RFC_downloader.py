190a12e0fa64a289b29a71716b8dbfb79b0a3fb5
John Widmaier <johnwid92@gmail.com>
2021-07-15 09:53:10 -0400

    changes to handle https and save output to file

--- RFC_downloader.py
+++ RFC_downloader.py
@@ -1,6 +1,4 @@
-import sys, socket
-
-print("Hello?")
+import os, sys, socket, ssl
 
 try:
     rfc_number = int(sys.argv[1])
@@ -8,24 +6,34 @@ except (IndexError, ValueError):
     print('Must supply an RFC number as first argument')
     sys.exit(2)
 
-print(rfc_number)
-
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
 host = 'www.ietf.org'
-port = 80
-sock = socket.create_connection((host, port))
+port = 443
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s_sock = context.wrap_socket(sock, server_hostname=host)
+s_sock.connect((host, port))
 
 req = (
-    f"""GET /rfc/rfc{rfc_number}.txt HTTP/1.1\r\nHost: {host}:{port}\r\n'User-Agent: Python {sys.version_info[0]}\r\nConnection: close\r\n\r\n"""
+f'GET /rfc/rfc{rfc_number}.txt HTTP/1.1\r\n'
+f'Host: {host}:{port}\r\n'
+f'User-Agent: Python {sys.version_info[0]}\r\n'
+'Connection: close\r\n'
+'\r\n'
 )
-print(req)
-sock.sendall(req.encode('ascii'))
-rfc_raw = bytearray()
 
+s_sock.send(req.encode('ascii'))
+rfc_raw = bytearray()
 while True:
-    buf = sock.recv(4096)
-    print(buf)
+    buf = s_sock.recv(4096)
     if not len(buf):
         break
     rfc_raw += buf
+
 rfc = rfc_raw.decode('utf-8')
 print(rfc)
+
+if not os.path.exists('./output'):
+    os.makedirs('./output')
+
+with open(f'./output/RFC_{rfc_number}.txt', "w") as outfile:
+    outfile.write(rfc)
